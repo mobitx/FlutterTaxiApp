1c7e90b356005708205b47d902d857b5d73e1422
zafrinah <zafrinah@itx360.com>
2020-06-08 14:18:51 +0530

    error handling and snackbar text

--- taxi_app/lib/main.dart
+++ taxi_app/lib/main.dart
@@ -1,133 +1,119 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
+void main() => runApp(MyApp());
+
+//class MyApp extends StatefulWidget {
+//  @override
+//  _State createState() => _State();
+//}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Login(),
+      ),
+    );
+  }
 }
 
-class MyApp extends StatefulWidget {
+class Login extends StatefulWidget {
   @override
-  _State createState() => _State();
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
 }
 
-class _State extends State<MyApp> {
-  TextEditingController usernameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  String usernameError, passwordError;
-
-  @override
-  void dispose() {
-    usernameController.dispose();
-    passwordController.dispose();
-    super.dispose();
-  }
-
-  String validate() {
-    if (usernameController.text.isEmpty) {
-      setState(() {
-        usernameError = "Please enter username";
-      });
-    }
-
-    if (passwordController.text.isEmpty) {
-      setState(() {
-        passwordError = "Please enter password";
-      });
-    }else{
-
-    }
-
-    return null;
-  }
-
-  void showToast() {
-    setState(() {
-      Fluttertoast.showToast(
-          msg: 'Some text',
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.CENTER,
-          backgroundColor: Colors.red,
-          textColor: Colors.white
-      );
-    });
-  }
+class MyCustomFormState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Padding(
-            padding: EdgeInsets.all(0),
-            child: ListView(
-              children: <Widget>[
-                Container(
-                  height: 300,
-                  decoration: BoxDecoration(
-                    color: Colors.indigoAccent,
-                    shape: BoxShape.rectangle,
-                  ),
-                  child: Center(
-                    child: Image.asset('assets/images/itx.jpg', height: 100),
-                  ),
+    return Form(
+      key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.indigoAccent,
+                shape: BoxShape.rectangle,
+              ),
+              child: Center(
+                child: Image.asset('assets/images/itx.jpg', height: 100),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
                 ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: TextField(
-                    controller: usernameController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Username',
-                      errorText: usernameError,
-                    ),
-                  ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter username';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
                 ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                      errorText: passwordError,
-                    ),
-                  ),
-                ),
-                FlatButton(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter password';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            FlatButton(
+              onPressed: (){
+                //forgot password screen
+              },
+              textColor: Colors.blue,
+              child: Text('Forgot Password'),
+            ),
+            Container(
+                height: 50,
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  color: Colors.blue,
+                  child: Text('LOGIN'),
                   onPressed: (){
-                    //forgot password screen
+                    if (_formKey.currentState.validate()) {
+                      Scaffold.of(context)
+                          .showSnackBar(SnackBar(content: Text('Login Successful')));
+                    }
                   },
-                  textColor: Colors.blue,
-                  child: Text('Forgot Password'),
-                ),
-                Container(
-                    height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: RaisedButton(
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      child: Text('LOGIN'),
-                      onPressed: showToast,
-                    )),
-                Container(
-                    child: Row(
-                      children: <Widget>[
-                        Text('Do not have an account?'),
-                        FlatButton(
-                          textColor: Colors.blue,
-                          child: Text(
-                            'Sign Up',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          onPressed: () {
-                            //signup screen
-                          },
-                        )
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.center,
-                    ))
-              ],
-            )));
+                )),
+            Container(
+                child: Row(
+                  children: <Widget>[
+                    Text('Do not have an account?'),
+                    FlatButton(
+                      textColor: Colors.blue,
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {
+                        //signup screen
+                      },
+                    )
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ))
+          ],
+        )
+    );
   }
 }
